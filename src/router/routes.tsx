c0ecdbe45b13118d7d69377c5855214d763d48ce
Phung Gia Bao <bao@aveapp.com>
2023-09-04 16:39:03 +0700

    remove some component test

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,13 +2,11 @@ import React, { lazy } from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 const LazyHomePage = lazy(() => import('@pages/HomePage/'))
-const LazyPageTest = lazy(() => import('@pages/PageSample/'))
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path='/home' element={<LazyHomePage />} />
-      <Route path='/pagetest' element={<LazyPageTest />} />
       <Route path='*' element={<LazyHomePage />} />
     </Routes>
   )
